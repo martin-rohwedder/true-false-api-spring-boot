04b567828fb047cb2ec5479c395d641185cba57d
martin-rohwedder <m.rohwedder@gmail.com>
2025-10-14 19:49:41 +0200

    Add FindAll and FindById endpoints for questions

--- src/main/java/dk/martinrohwedder/tfgapi/controllers/QuestionController.java
+++ src/main/java/dk/martinrohwedder/tfgapi/controllers/QuestionController.java
@@ -1,17 +1,33 @@
 package dk.martinrohwedder.tfgapi.controllers;
 
+import dk.martinrohwedder.tfgapi.entities.Question;
+import dk.martinrohwedder.tfgapi.repositories.QuestionRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@AllArgsConstructor
 @RestController
+@RequestMapping("/api/questions")
 public class QuestionController {
-    @GetMapping("/")
-    public String index() {
-        return "Hello from Spring Boot Application!!";
+
+    private final QuestionRepository questionRepository;
+
+    @GetMapping
+    public Iterable<Question> findAllQuestions() {
+        return questionRepository.findAll();
     }
 
-    @GetMapping("/test")
-    public String testEndpoint() {
-        return "This is a test endpoint";
+    @GetMapping("/{id}")
+    public ResponseEntity<Question> findQuestionById(@PathVariable Long id) {
+        var question = questionRepository.findById(id).orElse(null);
+        if (question == null) {
+            return ResponseEntity.notFound().build();
+        }
+
+        return ResponseEntity.ok(question);
     }
 }
