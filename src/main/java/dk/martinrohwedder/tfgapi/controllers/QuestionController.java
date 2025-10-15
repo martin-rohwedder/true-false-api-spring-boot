42ebabc2a5886c70b99c026eb48be3a57d83c4b3
martin-rohwedder <m.rohwedder@gmail.com>
2025-10-15 20:07:33 +0200

    Add Delete question method. + a few corrections.

--- src/main/java/dk/martinrohwedder/tfgapi/controllers/QuestionController.java
+++ src/main/java/dk/martinrohwedder/tfgapi/controllers/QuestionController.java
@@ -42,27 +42,27 @@ public class QuestionController {
 
     // GET /api/questions/{id}
     @GetMapping("/{id}")
-    public ResponseEntity<QuestionDto> findQuestionById(@PathVariable Long id) {
-        var question = questionRepository.findById(id).orElse(null);
-        if (question == null) {
+    public ResponseEntity<QuestionDto> findQuestionById(@PathVariable long id) {
+        var question = questionRepository.findById(id);
+        if (question.isEmpty()) {
             return ResponseEntity.notFound().build();
         }
 
-        return ResponseEntity.ok(questionMapper.toDto(question));
+        return ResponseEntity.ok(questionMapper.toDto(question.get()));
     }
 
     // POST: /api/questions
     @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<QuestionDto> addQuestion(@RequestBody AddQuestionRequest request, UriComponentsBuilder uriBuilder) {
         // Fetch category
-        var category = categoryRepository.findById(request.getCategoryId()).orElse(null);
-        if (category == null) {
+        var category = categoryRepository.findById(request.getCategoryId());
+        if (category.isEmpty()) {
             return ResponseEntity.badRequest().build();
         }
 
         // Map request to question and save it.
         var question = questionMapper.toEntity(request);
-        question.setCategory(category);
+        question.setCategory(category.get());
         questionRepository.save(question);
         var questionDto = questionMapper.toDto(question);
 
@@ -70,5 +70,17 @@ public class QuestionController {
         var uriLocation = uriBuilder.path("/api/questions/{id}").buildAndExpand(questionDto.getId()).toUri();
 
         return ResponseEntity.created(uriLocation).body(questionDto);
+    }
+
+    // DELETE: /api/questions/{id}
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> removeQuestion(@PathVariable long id) {
+        var question = questionRepository.findById(id);
+        if (question.isPresent()) {
+            questionRepository.delete(question.get());
+            return ResponseEntity.noContent().build();
+        }
+
+        return ResponseEntity.notFound().build();
     }
 }
